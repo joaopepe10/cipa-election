a1b5caa5d30d18d540b0a16a44c5b8e64d5097a7
João Pedro Pires Barçante <joaopedropires61@gmail.com>
2024-09-05 21:47:10 -0300

    feat: create migration to user

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# CRIAR MODELO BANCO DE DADOS
+# py manage.py makemigrations
+
+#GERAR DADOS
+# py manage.py migrate
+
+class User(models.Model):
+
+    user_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50, default='')
+    email = models.CharField(max_length=50, default='')
+
+    def __str__(self):
+        return f'user_id: {self.user_id}, name: {self.name}, email: {self.email}'
