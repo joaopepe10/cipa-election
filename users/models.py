41fc91ecfb24c97cbc5a267ae7e65d2a789b21a2
João Pedro Pires Barçante <joaopedropires61@gmail.com>
2024-09-30 21:19:16 -0300

    feat: add migration

--- users/models.py
+++ users/models.py
@@ -6,10 +6,13 @@ from django.db import models
 #GERAR DADOS
 # py manage.py migrate
 
+#RUN PROJECT
+# python manage.py runserver
+
 class User(models.Model):
     user_id = models.IntegerField(primary_key=True, editable=False)
     name = models.CharField(max_length=50, default='')
-    email = models.CharField(max_length=50, default='')
+    email = models.CharField(max_length=50, default='', unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
