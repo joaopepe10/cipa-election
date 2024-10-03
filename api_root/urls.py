06511320e18f233900ff9554eaf8781d71b2080f
João Pedro Pires Barçante <joaopedropires61@gmail.com>
2024-10-02 21:20:07 -0300

    feat: create HttpMethod Get

--- api_root/urls.py
+++ api_root/urls.py
@@ -1,32 +1,6 @@
-"""
-URL configuration for api_root project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework import routers
-from users.api import viewsets as userviewsets
-
-router = routers.DefaultRouter()
-
-router.register(r'users', userviewsets.UserViewSet, basename='Users')
-router.register(r'candidates', userviewsets.UserViewSet, basename='Candidates')
-router.register(r'elections', userviewsets.UserViewSet, basename='Elections')
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    # path('admin/', admin.site.urls),
+    path('api/', include('users.api.urls'), name='api_rest_urls'),
 ]
--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from users import models
 
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
-        fields = 'name', 'email'
+        fields = ['name', 'email']
+
+class CandidateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Candidate
+        fields = ['user', 'speech']
