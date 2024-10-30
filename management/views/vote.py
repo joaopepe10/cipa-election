86683ef523c94183fb55f3fcc116026264b0a498
João Pedro Pires Barçante <joaopedropires61@gmail.com>
2024-10-29 23:20:22 -0300

    feat: formatar erros

--- management/views/vote.py
+++ management/views/vote.py
@@ -12,21 +12,29 @@ from management.serialiazers.serializers import VoteSerializer, SendVoteCandidat
 def post_vote(request):
     if request.method == 'POST':
         dto = SendVoteCandidateSerializer(data=request.data)
-        if not dto.is_valid() and Vote.objects.filter(user_id=dto['user'], election_id=dto['election']).exists():
+
+        if not dto.is_valid():
             return Response(
-            {
-                'error': 'User already voted',
-                'details': dto.errors
-            },
-            status=status.HTTP_400_BAD_REQUEST
-        )
+                {
+                    'error': 'Invalid data',
+                    'details': dto.errors
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        if Vote.objects.filter(user_id=dto.validated_data['user'], election_id=dto.validated_data['election']).exists():
+            return Response(
+                {
+                    'error': 'User already voted',
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
         user = get_object_or_404(models.User, pk=dto.validated_data['user'])
         election = get_object_or_404(models.Election, pk=dto.validated_data['election'])
         candidate = get_object_or_404(models.Candidate, pk=dto.validated_data['candidate'])
 
         vote = Vote.objects.create(user=user, election=election, candidate=candidate)
-        vote.save()
+
         response = VoteSerializer(vote).data
         return Response(data=response, status=status.HTTP_201_CREATED)
 
